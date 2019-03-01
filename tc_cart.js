90116643eb12967cded9ac668d5a00e225956182
Evelyn Duarte <eduarte019@west-mec.org>
2019-02-28 18:25:07 -0700

    Update 2/28
    Made 4 arrays for value of items. Also set a variable for an initial value. HTML count as well.

---
+++ tc_cart.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+
+   Author: Evelyn Duarte
+   Date:  2/28/19
+   
+   Filename: tc_cart.js
+	
+*/
+
+// The variable or orderTotal has the initial value of 0.
+var orderTotal = 0;
+
+cartHTML = "<table> <tr> <th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th> </tr>";
---
+++ tc_order.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+  
+   Author: Evelyn Duarte
+   Date: 2/28/19
+     
+   Filename: tc_order.js  
+
+   The item array contains the ID numbers of the items ordered by the customer
+   The itemDescription array contains the description of each item
+   The itemPrice array contains the price of each item
+   The itemQty array contains the quantity ordered of each item
+   
+*/
+// An array of item is made to have the numbers of the items.
+var item = [10582, 23015, 41807, 10041];
+// An array with text strings of all the available things to be purchased.
+var itemDescription = ["1975 Green Bay Packers Football (signed)", " Tom Landry 1955 Football Card (unsigned)", "1916 Army-Navy Game, Framed Photo (signed)", " Protective Card Sheets"];
+// Also arrays with the numbers.
+var itemPrice = [149.93, 89.98, 334.93, 22.67];
+
+var itemQty = [1, 1, 1, 4];
